eddc21832753fe708d1dc7b0100e60e4de38c5a0
Nyx <nyx@mindscape.world>
2016-06-07 12:38:18 +0800

    Begin

---
+++ Assets/Start_Script.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Start_Script : MonoBehaviour {
+
+	public void LoadScene()
+	{
+		Debug.Log("Loading First Scene");
+		SceneManager.LoadScene("first", LoadSceneMode.Single);
+	}
+}
